1a0c74f44213d2ec347096889ddc35801eaf35b1
Erko Bridee <erko.bridee@gmail.com>
2020-02-19 09:35:29 +0100

    feat(libs): add react-hot-loader to the todos wrapper lib

--- libs/todos/wrapper/src/lib/index.tsx
+++ libs/todos/wrapper/src/lib/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { hot } from 'react-hot-loader/root';
 
 import ReduxWrapper from './Redux';
 
@@ -6,4 +7,4 @@ const RootWrapper: React.FunctionComponent = ({ children }) => (
   <ReduxWrapper>{children}</ReduxWrapper>
 );
 
-export default RootWrapper;
+export default hot(RootWrapper);
