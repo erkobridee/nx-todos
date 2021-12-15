e58f5809cce27c11cfe60d10f13122e5fc22533b
Erko Bridee <erko.bridee@gmail.com>
2021-12-15 10:49:36 +0100

    update(nx): 2nd step migrating into v13.1.0

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/todos',
-    '<rootDir>/apps/api',
-    '<rootDir>/libs/shared/data-model',
-    '<rootDir>/libs/api/router',
-    '<rootDir>/libs/api/data',
-    '<rootDir>/libs/todos/api-calls',
-    '<rootDir>/libs/shared/helpers',
-    '<rootDir>/libs/shared/redux-helpers',
-    '<rootDir>/libs/todos/redux',
-    '<rootDir>/libs/todos/wrapper',
-    '<rootDir>/libs/todos/ui-components',
-    '<rootDir>/libs/todos/feature',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
